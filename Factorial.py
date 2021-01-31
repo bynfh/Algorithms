d23ea822561fa5e66f6b1a7b6f3f055f74c9320a
MarkovOA <markov-oa@mail.ri>
2021-01-31 17:10:41 +0500

    Added Factorial.py and Fibbonachi_cache.py

---
+++ Factorial.py
@@ -0,0 +1,22 @@
+"""Function with recursion restrict count of call. Max count of call == 998
+   Function with loop don't have restrict in call"""
+
+
+def FactorialRecursion(numb: int) -> int:
+    """Function calculate factorial numb. Max numb == 998"""
+    if numb == 0 or numb == 1:
+        return 1
+    return FactorialRecursion(numb - 1) * numb
+
+
+def FactorialLoop(numb: int) -> int:
+    """Function calculate factorial numb"""
+    factorial = 1
+    while numb > 1:
+        factorial *= numb
+        numb -= 1
+    return factorial
+
+
+print(FactorialLoop(10000))
+print(FactorialLoop(998))
