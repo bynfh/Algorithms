dc03c4e251a3682ca5a48148d21b7fa6251a52ac
MarkovOA <markov-oa@mail.ri>
2021-03-06 18:36:39 +0500

    Added iter, repr in Linked List

---
+++ LinkedListWithIter.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    flag_stop_iter = False
+
+    def __init__(self, data):
+        self.node = Node(data)
+
+    def append(self, value):
+        end = Node(value)
+        n = self.node
+
+        while n.next:
+            n = n.next
+
+        n.next = end
+
+    def __repr__(self):
+        result = []
+        n = self.node
+        result.append(n.data)
+
+        while n.next:
+            n = n.next
+            result.append(n.data)
+
+        return f"{result}"
+
+    def __next__(self):
+        if self.node.next:
+            result = self.node.data
+            self.node = self.node.next
+            return result
+
+        if self.flag_stop_iter:
+            raise StopIteration
+
+        result = self.node.data
+        self.flag_stop_iter = True
+
+        return result
+
+    def __iter__(self):
+        return self
+
+
+Test = LinkedList(1)
+Test.append(2)
+Test.append(3)
+
+for _ in Test:
+    print(_)
+
